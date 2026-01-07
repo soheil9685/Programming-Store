b735a67cb3ec85fce7778e13ce1d311af1e150e8
SoheilHosseini <canopus.soheilhosseini@gmail.com>
2026-01-07 14:08:26 +0330

    Change the direction into RTL

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.rtl.min.css';
+import { ThemeProvider } from 'react-bootstrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <ThemeProvider dir="rtl">
+    <App />
+  </ThemeProvider>
 );
