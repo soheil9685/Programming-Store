f7b83879a8e8108e9a34c278ca29ee23fb83fd1f
SoheilHosseini <canopus.soheilhosseini@gmail.com>
2026-02-15 09:44:22 +0330

    Create Course component

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,8 +1,38 @@
 import "./Course.css"
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { IoMdTime } from "react-icons/io";
+import { PiUsersThreeFill } from "react-icons/pi";
 
-function Course(){
-    return(
-        <></>
+function Course() {
+    return (
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="https://dl.next1code.ir/images/courses/typescript.png" />
+            <span className="students estedad d-flex align-items-center justify-content-between"> 
+                <PiUsersThreeFill className="fs-5"/>
+                215
+            </span>
+            <Card.Body>
+                <Card.Title className="lalezar fs-3">موضوع</Card.Title>
+                <Card.Text className="estedad my-4">
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است
+                </Card.Text>
+                <hr />
+                <div className="cardFooter">
+                    <p className="estedad d-flex justify-content-between">
+                        <span>
+                            <FaChalkboardTeacher className="fs-4" />
+                            : سهیل حسینی
+                        </span>
+                        <span>
+                            <IoMdTime className="fs-4" />
+                            : 50 دقیقه
+                        </span>
+                    </p>
+                </div>
+            </Card.Body>
+        </Card>
     )
 }
 
--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -5,8 +5,11 @@ import HeaderImg from "../../assets/Image/Header.jpg"
 import Aos from "aos";
 import "aos/dist/aos.css"
 import { useEffect } from "react";
+import Course from "../../components/Course/Course";
 
 function Home() {
+
+    
 
     useEffect(() => {
         Aos.init();
@@ -33,6 +36,12 @@ function Home() {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                 <path fill="#9BA4B4" fill-opacity="1" d="M0,160L48,181.3C96,203,192,245,288,229.3C384,213,480,139,576,117.3C672,96,768,128,864,122.7C960,117,1056,75,1152,74.7C1248,75,1344,117,1392,138.7L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
             </svg>
+            <section id="courses">
+                <Container>
+                    <h2 className="lalezar mb-4">دوره های آموزشی</h2>
+                    <Course />
+                </Container>
+            </section>
         </>
     )
 }
